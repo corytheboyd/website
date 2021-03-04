6b75aab8ba502bf95732166afc77798a66f2b513
Cory Boyd <me@corytheboyd.com>
2021-03-04 16:34:10 -0700

    HMR!

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1 className="text-lg">Hello, world!</h1>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,6 @@ import { render } from "react-dom";
 import { App } from "./App";
 
 render(<App />, document.getElementById("root"));
+
+// @ts-ignore
+if (module.hot) module.hot.accept();
