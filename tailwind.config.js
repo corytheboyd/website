f9d3fe04986cce59306f935572364dd360af148e
Cory Boyd <me@corytheboyd.com>
2022-04-28 10:35:35 -0600

    Upgrade TailwindCSS to 3.x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,4 @@
 module.exports = {
-  content: ["./src/**/*.html", "./src/**/*.tsx"],
+  content: ["./src/**/*.html"],
   plugins: [require("@tailwindcss/typography")],
-  theme: {
-    extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme("colors.gray.200"),
-            h1: {
-              color: theme("colors.pink.600"),
-            },
-            h2: {
-              color: theme("colors.pink.600"),
-            },
-            h3: {
-              color: theme("colors.pink.600"),
-            },
-            h4: {
-              color: theme("colors.pink.600"),
-            },
-            code: {
-              color: theme("colors.purple.500"),
-            },
-            strong: {
-              color: theme("colors.gray.400"),
-            },
-          },
-        },
-      }),
-    },
-  },
 };
