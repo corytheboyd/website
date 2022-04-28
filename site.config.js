ae78d3dee870f5fdc47b2fa6f267665e3681c2e9
Cory Boyd <me@corytheboyd.com>
2022-04-28 10:30:00 -0600

    Add blog + remove projects
    Projects were dead. Go look at my GitHub page, I pin my projects there.

--- site.config.js
+++ site.config.js
@@ -2,41 +2,15 @@ const { readFileSync } = require("fs");
 
 module.exports = {
   posts: {
-    // prettier-ignore
-    '2020-03-09': {
+    "2022-04-28": {
+      date: "2022-04-28",
+      title: "It still works!",
+      content: readFileSync("./src/posts/2022-04-28.md").toString(),
+    },
+    "2020-03-09": {
       date: "2020-03-09",
       title: "New city, new job, new... website?",
       content: readFileSync("./src/posts/2020-03-09.md").toString(),
-    },
-  },
-  projects: {
-    midishare: {
-      title: "Midishare",
-      content: readFileSync("./src/projects/midishare.md").toString(),
-      links: {
-        www: {
-          url: "https://www.midishare.app",
-          label: "www.midishare.app",
-        },
-        github: {
-          url: "https://github.com/corytheboyd/midishare",
-          label: "github.com/corytheboyd/midishare",
-        },
-      },
-    },
-    website: {
-      title: "Website",
-      content: readFileSync("./src/projects/website.md").toString(),
-      links: {
-        www: {
-          url: "https://corytheboyd.com",
-          label: "corytheboyd.com",
-        },
-        github: {
-          url: "https://github.com/corytheboyd/website",
-          label: "github.com/corytheboyd/website",
-        },
-      },
     },
   },
 };
