561d606bf281c9fe1eb4f8fe4c9550277371fd32
Cory Boyd <me@corytheboyd.com>
2022-04-28 12:41:28 -0600

    Add FontAwesome

--- site.config.js
+++ site.config.js
@@ -1,16 +1,1 @@
-const { readFileSync } = require("fs");
-
-module.exports = {
-  posts: {
-    "2022-04-28": {
-      date: "2022-04-28",
-      title: "It still works!",
-      content: readFileSync("./src/posts/2022-04-28.md").toString(),
-    },
-    "2020-03-09": {
-      date: "2020-03-09",
-      title: "New city, new job, new... website?",
-      content: readFileSync("./src/posts/2020-03-09.md").toString(),
-    },
-  },
-};
+module.exports = {};
