669c1ada876e1efe4554324dc758fa0322f9d742
Cory Boyd <me@corytheboyd.com>
2023-07-02 13:35:19 -0600

    First pass at some content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello, js")
+import anime from './lib/anime.es.js';
+
+const targets = document.getElementsByClassName("anime")
+
+anime({
+    targets: targets,
+    translateX: 100,
+    duration: 500,
+});
