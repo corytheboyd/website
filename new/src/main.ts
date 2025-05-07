dc5a6d24385d7e0b7094132b4b327e381f7e713c
Cory Boyd <me@corytheboyd.com>
2025-05-07 15:46:53 -0600

    fuck it this is glorious solitaire ACTION

--- new/src/main.ts
+++ new/src/main.ts
@@ -2,6 +2,7 @@
 import "./components/earth-globe.ts";
 import "./components/picture-frame.ts";
 import "./components/satellite-flyby.ts";
+import "./components/solitaire-win.ts";
 import "./components/star-bg.ts";
 import "./components/win98-icon.ts";
 import "./components/win98-link-icon.ts";
