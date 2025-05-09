c0f0bbb65e3d0f133e6a6e8768eb6b40452fd0c1
Cory Boyd <me@corytheboyd.com>
2025-05-09 11:06:17 -0600

    WIP desktop window sizing

--- new/src/App.vue
+++ new/src/App.vue
@@ -1,53 +1,9 @@
 <template>
-  <template v-if="!isMobile">
-    <Starscape class="fixed inset-0 w-full h-full z-0" />
-    <SolitaireMadness class="fixed inset-0 w-full h-full z-10" />
-  </template>
-  <div class="relative z-20 h-full">
-    <!-- DESKTOP -->
-    <div
-      :class="[
-        'background-windows-teal',
-        'shadow-[0_30px_70px_rgba(0,0,0,0.7)]',
-        'md:aspect-[4/3]',
-        'h-full',
-        'flex flex-col items-center relative',
-        'overflow-hidden',
-      ]"
-    >
-      <WelcomeWindow />
-      <SocialIcons />
-      <SocialLinks />
-      <TaskbarFooter />
-    </div>
-  </div>
+  <Background />
+  <Desktop />
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from "vue";
-import WelcomeWindow from "./components/WelcomeWindow.vue";
-import SocialIcons from "./components/SocialIcons.vue";
-import SocialLinks from "./components/SocialLinks.vue";
-import EarthWindow from "./components/EarthWindow.vue";
-import TaskbarFooter from "./components/TaskbarFooter.vue";
-import Starscape from "./components/custom/Starscape.vue";
-import SolitaireMadness from "./components/custom/SolitaireMadness.vue";
-
-const isMobile = ref(false);
-
-const checkMobile = () => {
-  isMobile.value = window.innerWidth < 768;
-};
-
-onMounted(() => {
-  checkMobile();
-  window.addEventListener("resize", checkMobile);
-});
-onUnmounted(() => {
-  window.removeEventListener("resize", checkMobile);
-});
+import Background from "./components/Background.vue";
+import Desktop from "./components/Desktop.vue";
 </script>
-
-<style>
-/* Global styles will be imported from style.css */
-</style>
---
+++ new/src/components/Background.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hidden md:block">
+    <Starscape class="fixed inset-0 w-full h-full z-0" />
+    <SolitaireMadness class="fixed inset-0 w-full h-full z-10" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Starscape from "./custom/Starscape.vue";
+import SolitaireMadness from "./custom/SolitaireMadness.vue";
+</script>
