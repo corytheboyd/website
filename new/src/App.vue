ece1c327cc2bb2dbda20abeab785d6b0194dbfaa
Cory Boyd <me@corytheboyd.com>
2025-05-16 10:57:50 -0600

    WIP proper flex for desktop/taskbar

--- new/src/App.vue
+++ new/src/App.vue
@@ -1,20 +1,9 @@
 <template>
-  <div class="no-select">
-    <BackgroundContainer />
-    <DesktopContainer />
-  </div>
+  <BackgroundContainer />
+  <DesktopContainer />
 </template>
 
 <script setup lang="ts">
 import BackgroundContainer from "@/components/BackgroundContainer.vue";
 import DesktopContainer from "@/components/DesktopContainer.vue";
 </script>
-
-<style>
-.no-select {
-  user-select: none;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-}
-</style>
