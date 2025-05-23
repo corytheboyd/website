868a18dc7747e63b97a6c16fbe582e3166705039
Cory Boyd <me@corytheboyd.com>
2025-05-23 12:28:41 -0700

    Add current time to toolbar info

--- src/components/ToolbarInfo.vue
+++ src/components/ToolbarInfo.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="status-bar">
-    <p class="status-bar-field">
-      <img src="/nyan.gif" alt="Nyan Cat" class="w-12" />
+    <p class="status-bar-field flex items-center justify-center">
+      {{ currentTime }}
     </p>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+const currentTime = ref("");
+
+function updateTime() {
+  const now = new Date();
+  currentTime.value = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
+
+let timer;
+
+onMounted(() => {
+  updateTime();
+  timer = setInterval(updateTime, 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(timer);
+});
+</script>
